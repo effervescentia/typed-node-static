4344a6763dc135cb65d3361c828f8c73e8b9857f
Ben Teichman <ben.l.teichman@gmail.com>
2016-06-09 14:30:56 -0400

    initial

---
+++ index.d.ts
@@ -0,0 +1,40 @@
+import { ServerRequest, ServerResponse } from 'http';
+import { Stats } from 'fs';
+import mimeType = require('mime');
+
+declare namespace NodeStatic {
+  var version: number[];
+  var mime: typeof mimeType.Mime;
+
+  class Server {
+    constructor(path?: string, options?: ServerOptions);
+
+    server(request: ServerRequest, response: ServerResponse, cb?: (err: Error, result: any) => void): void;
+    serveFile(filePath: string, status: number, headers: any, request: ServerRequest, response: ServerResponse): void;
+    serveDir(dirPath: string, request: ServerRequest, response: ServerResponse, cb: FinishResponse): void;
+    servePath(path: string, status: number, headers: any, request: ServerRequest, response: ServerResponse, cb: FinishResponse): void;
+    resolve(path: string): void;
+    serve(req: ServerRequest, res: ServerResponse, cb?: (err: Error, result: any) => void): void;
+    gzipOk(req: ServerRequest, contentType: string): void;
+    respondGzip(path: string, status: number, contentType: string, headers: any, files: string[], stat: Stats, req: ServerRequest, res: ServerResponse, finish: FinishResponse): void;
+    respondNoGzip(path: string, status: number, contentType: string, headers: any, files: string[], stat: Stats, req: ServerRequest, res: ServerResponse, finish: FinishResponse): void;
+    respond(path: string, status: number, headers: any, files: string[], stat: Stats, req: ServerRequest, res: ServerResponse, finish: FinishResponse): void;
+    stream(path: string, files: string[], length: number, startByte: number, res: ServerResponse, cb?: (err: Error, offset: number) => void): void;
+    parseByteRange(req: ServerRequest, stat: Stats): void;
+    finish(status: number, headers: {}, req: ServerRequest, res: ServerResponse, promise: Promise<any>, cb?: (err: Error, result: any) => void): void;
+  }
+
+  interface FinishResponse {
+    (status: number, headers: any): void;
+  }
+
+  interface ServerOptions {
+    cache?: number;
+    serverInfo?: string;
+    headers?: any;
+    gzip?: boolean | RegExp;
+    indexFile?: string;
+  }
+}
+
+export = NodeStatic;
---
+++ test/test.ts
@@ -0,0 +1,14 @@
+import test = require('blue-tape');
+
+import nodeStatic = require('node-static');
+
+test('sails exists', (t) => {
+  t.plan(3);
+  t.notEqual(nodeStatic, undefined);
+  let Server = nodeStatic.Server;
+
+  t.notEqual(Server, undefined);
+  let staticServer = new Server();
+
+  t.notEqual(staticServer, undefined);
+});
